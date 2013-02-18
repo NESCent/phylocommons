b91e5ca7384f3de6a8c7bc04a75e3724b8f108b6
bendmorris <ben@bendmorris.com>
2013-02-18 11:36:28 -0500

    Some basic setup and stub URLs.

--- phylofile/settings.py
+++ phylofile/settings.py
@@ -1,5 +1,9 @@
 # Django settings for phylofile project.
 
+import os
+
+ABS_PATH = '/path/to/django/app/'
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,7 +15,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -24,7 +28,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'US/Eastern'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -116,9 +120,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
--- phylofile/urls.py
+++ phylofile/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'phylofile.views.home', name='home'),
-    # url(r'^phylofile/', include('phylofile.foo.urls')),
-
+    url(r'^$', 'phylofile.views.home', name='home'),
+    url(r'^about/$', 'phylofile.views.about', name='about'),
+    
+    # trees application (add, get, query, etc.)
+    url(r'^trees/', include('phylofile.trees.urls')),
+    
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
