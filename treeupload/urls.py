06d1e12978991482d7cacae7f3e8aa297700014e
bendmorris <ben@bendmorris.com>
2013-03-05 11:52:58 -0500

    View and test potential submissions.

--- treeupload/urls.py
+++ treeupload/urls.py
@@ -5,4 +5,8 @@ urlpatterns = patterns('',
     url(r'^add/$', 'treeupload.views.add', name='add'),
     url(r'^uploads/$', 'treeupload.views.uploads', name='uploads'),
     url(r'^uploads/view/(?P<tree_id>[^/]*)/$', 'treeupload.views.upload_view', name='upload_view'),
+    url(r'^uploads/test/(?P<tree_id>[^/]*)/$', 'treeupload.views.upload_test', name='upload_test'),
+    url(r'^uploads/approve/(?P<tree_id>[^/]*)/$', 'treeupload.views.upload_approve', name='upload_approve'),
+    url(r'^uploads/reject/(?P<tree_id>[^/]*)/$', 'treeupload.views.upload_reject', name='upload_reject'),
+    url(r'^uploads/(?P<tree_id>[^/]*)/$', 'treeupload.views.upload_action', name='upload_action'),
 )
--- treeupload/views.py
+++ treeupload/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
@@ -80,3 +80,43 @@ def upload_view(request, tree_id):
     response.write(open(file_path).read())
 
     return response
+    
+    
+@login_required
+def upload_test(request, tree_id):
+    submission = TreeSubmission.objects.get(id=tree_id)
+    file_path = submission.tree_file.path
+    
+    try:
+        tree = bp.read(file_path, submission.format)
+        s = StringIO()
+        bp.write(tree, s, 'newick')
+        text = '# SUCCESS\n%s' % s.getvalue()
+    except Exception as e:
+        text = '# ERROR: %s' % e
+    
+    response = HttpResponse(mimetype='text/plain')
+    response.write(text)
+
+    return response
+    
+    
+@login_required
+def upload_approve(request, tree_id):
+    pass
+    
+    
+@login_required
+def upload_reject(request, tree_id):
+    pass
+    
+    
+@login_required
+def upload_action(request, tree_id):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'View file': return redirect('upload_view', tree_id=tree_id)
+        elif action == 'Test file': return redirect('upload_test', tree_id=tree_id)
+        elif action == 'Approve': return redirect('upload_approve', tree_id=tree_id)
+        elif action == 'Reject': return redirect('upload_reject', tree_id=tree_id)
+        else: return redirect('uploads')
