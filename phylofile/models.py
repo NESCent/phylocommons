bc5b81742c8fe0bf2a096fd56e6d834e3b7894fc
bendmorris <ben@bendmorris.com>
2013-03-05 12:07:42 -0500

    Break up lines within <pre> tags in user profiles.

--- phylofile/models.py
+++ phylofile/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from phylofile.signals import create_profile
+from django.utils.html import escape
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     profile_text = models.TextField(max_length=5000)
+    
+    #def profile_text_html(self):
+        #return escape(self.profile_text).replace('\n', '<br/>')
     
     
 models.signals.post_save.connect(create_profile, sender=User)
