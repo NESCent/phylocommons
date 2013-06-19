83610b2ef61ef8eb864778e01a3d823952923712
bendmorris <ben@bendmorris.com>
2013-06-19 11:13:32 -0400

    By default, don't use a taxonomy for TreeBASE trees as this would take forever and not work very well.

--- tools/treebase_scraper/ingest_trees.py
+++ tools/treebase_scraper/ingest_trees.py
@@ -4,23 +4,32 @@ from treestore import Treestore
 
 
 try: taxonomy = sys.argv[1]
-except: taxonomy = 'itis'
+except: taxonomy = None
 
 t = Treestore()
 
 tree_files = [x for x in os.listdir('trees') if x.endswith('.nex')]
 base_uri = 'http://www.phylocommons.org/trees/%s'
 tree_list = t.list_trees()
-sys.stdout.write('Loading taxonomy...')
-sys.stdout.flush()
-taxonomy = t.get_trees('http://www.phylocommons.org/trees/%s_taxonomy' % taxonomy)[0]
-print 'done.'
+if taxonomy:
+    sys.stdout.write('Loading taxonomy...')
+    sys.stdout.flush()
+    taxonomy = t.get_trees('http://www.phylocommons.org/trees/%s_taxonomy' % taxonomy)[0]
+    taxonomy.index_labels()
+    print 'done.'
 
+errors = set()
 for tree_file in tree_files:
-    tree_id = 'TB2_' + tree_file[:len('.nex')]
+    tree_id = 'TB2_' + tree_file[:-len('.nex')]
     tree_uri = base_uri % tree_id
     if tree_uri in tree_list: continue
     print '**', tree_uri
     tree_path = os.path.join('trees', tree_file)
-    t.add_trees(tree_path, 'nexus', tree_uri=tree_uri, rooted=False,
-                taxonomy=taxonomy, tax_root=None)
+    try:
+        t.add_trees(tree_path, 'nexus', tree_uri=tree_uri, rooted=False,
+                    taxonomy=taxonomy, tax_root=None)
+    except:
+        errors.add(tree_id)
+
+if errors:
+    print "Couldn't load the following trees:", ','.join(errors)
