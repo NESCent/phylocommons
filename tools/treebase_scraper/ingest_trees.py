160b6772bb706f89cc384ab389812cd3dff409f6
bendmorris <ben@bendmorris.com>
2013-06-22 16:56:03 -0400

    Don't hardcode URI into ingest_trees.py

--- tools/treebase_scraper/ingest_trees.py
+++ tools/treebase_scraper/ingest_trees.py
@@ -15,22 +15,21 @@ tree_list = set(t.list_trees())
 if taxonomy:
     sys.stdout.write('Loading taxonomy...')
     sys.stdout.flush()
-    taxonomy = t.get_trees('http://www.phylocommons.org/trees/%s_taxonomy' % taxonomy)[0]
+    taxonomy = t.get_trees('%s_taxonomy' % taxonomy)[0]
     taxonomy.index_labels()
     print 'done.'
 
 errors = set()
 for tree_file in tree_files:
     tree_id = 'TB2_' + tree_file[:-len('.nex')]
-    tree_uri = base_uri % tree_id
-    if tree_uri in tree_list: continue
-    print '**', tree_uri
+    if tree_id in tree_list: continue
+    print '**', tree_id
     tree_path = os.path.join('trees', tree_file)
     with open(tree_path) as input_file:
         r = input_file.read()
     if '<!DOCTYPE html' in r: continue
     try:
-        t.add_trees(tree_path, 'nexus', tree_uri=tree_uri, rooted=False,
+        t.add_trees(tree_path, 'nexus', tree_uri=tree_id, rooted=False,
                     taxonomy=taxonomy, tax_root=None)
     except Exception as e:
         print 'ERROR: ', e
