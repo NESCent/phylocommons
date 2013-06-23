c0d7794cdec6362d66abc666d2ad1793121f1768
bendmorris <ben@bendmorris.com>
2013-06-22 23:32:25 -0400

    Annotate TreeBASE trees, for now just by citing their URI on TreeBASE.

---
+++ tools/treebase_scraper/annotate_trees.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import os
+from treestore import Treestore
+
+
+try: taxonomy = sys.argv[1]
+except: taxonomy = None
+
+t = Treestore()
+
+treebase_uri = 'http://purl.org/phylo/treebase/phylows/tree/%s'
+
+tree_files = [x for x in os.listdir('trees') if x.endswith('.nex')]
+base_uri = 'http://www.phylocommons.org/trees/%s'
+tree_list = set(t.list_trees())
+for tree_uri in tree_list:
+    if not 'TB2_' in tree_uri: pass
+    tree_id = t.id_from_uri(tree_uri)
+    tb_uri = treebase_uri % (tree_id.replace('_', ':'))
+    print tree_id, tb_uri
+    t.annotate(tree_uri, annotations='?tree bibo:cites <%s> .' % tb_uri)
