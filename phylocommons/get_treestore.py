49ea84e42fa95e32f89453e9ecd416a2cd55f7fd
bendmorris <ben@bendmorris.com>
2013-06-22 21:35:14 -0400

    Use treestore's id_from_uri method.

--- phylocommons/get_treestore.py
+++ phylocommons/get_treestore.py
@@ -9,7 +9,4 @@ def uri_from_tree_id(tree_id):
     return Treestore.uri_from_id(tree_id, base_uri=settings.TREE_URI)
 
 def tree_id_from_uri(uri):
-    if uri.startswith(settings.TREE_URI):
-        uri = uri.replace(settings.TREE_URI, '', 1)
-    if uri.endswith('/'): uri = uri.rstrip('/')
-    return uri
+    return Treestore.id_from_uri(uri, base_uri=settings.TREE_URI)
