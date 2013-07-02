f8c1aeb544937052534263a2335223f2603860b6
bendmorris <ben@bendmorris.com>
2013-07-02 13:10:52 -0400

    Use markdown for user profiles.

--- phylocommons/models.py
+++ phylocommons/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from phylocommons.signals import create_profile
 from django.utils.html import escape
+from markdown import markdown
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     profile_text = models.TextField(max_length=5000)
     
-    #def profile_text_html(self):
-        #return escape(self.profile_text).replace('\n', '<br/>')
+    def profile_text_html(self):
+        return markdown(self.profile_text)
     
     
 models.signals.post_save.connect(create_profile, sender=User)
