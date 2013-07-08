c5573187cee19fa84e375deacf0f630b5fba15bd
bendmorris <ben@bendmorris.com>
2013-07-08 10:40:25 -0400

    A number is expected here; not providing one causes an exception.

--- phylocommons/request_logger.py
+++ phylocommons/request_logger.py
@@ -15,6 +15,6 @@ class LoggingMiddleware(object):
             time.strftime("%a %m/%d/%y %I:%M:%S %p", time.localtime()),
             response.status_code,
             request.get_full_path(),
-            (time.time() - request.timer) if hasattr(request, 'timer') else 'err'
+            (time.time() - request.timer) if hasattr(request, 'timer') else -1
         )
         return response
